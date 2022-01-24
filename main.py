bf3ff6ac1d742506cd1653bcd23ad2d480658579
Islam <35372062+Skyt1ess@users.noreply.github.com>
2022-01-24 10:42:32 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+import math
+
+def is_prime(x):
+    if x < 2:
+        return False
+    for i in range(2, int(math.sqrt(x)) + 1):
+        if x % i == 0:
+            return False
+    return True
+
+for i in range(100):
+    if is_prime(i):
+        print(i)
